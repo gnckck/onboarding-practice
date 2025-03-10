494e93830dca78b479b7dbdfe9b70eb7cd904c5b
jina <jjnwlsk@naver.com>
2025-03-10 14:28:39 +0900

    feat : 포켓몬 목록 호출 시 로딩스피너 적용

--- src/pages/PokemonPage.tsx
+++ src/pages/PokemonPage.tsx
@@ -5,6 +5,7 @@ import { InformationCircleIcon } from "@heroicons/react/24/outline";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { minimumLoadingTime } from "@/lib/mininumLoadingTime";
+import { LoadingSpinner } from "@/components/common/LoadingSpinner";
 
 export default function PokemonPage() {
   const [selectedPokemon, setSelectedPokemon] = useState<string | null>(null);
@@ -22,6 +23,8 @@ export default function PokemonPage() {
         setPokemonList(pokemons);
       } catch (error) {
         console.error("", error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchPokemonList();
@@ -40,29 +43,34 @@ export default function PokemonPage() {
   const fetchPokemonInfo = async (pokemonUrl: string) => {
     const response = await axios.get(pokemonUrl);
     setPokemonDetail(response.data);
-    console.log(response.data);
   };
 
   return (
     <div className="p-5 flex h-[50vh] justify-center">
-      <Card className="p-5 w-1/3 mr-5">
+      <Card className="p-5 w-150 mr-5">
         <h2 className="text-2xl font-bold ">포켓몬 목록</h2>
-        <div className="flex flex-wrap gap-2">
-          {pokemonList.map((pokemon) => (
-            <Badge
-              key={pokemon.name}
-              variant="outline"
-              className={`m-1 cursor-pointer hover:bg-gray-100 ${
-                selectedPokemon === pokemon.name ? "border-black" : ""
-              } `}
-              onClick={() => togglePokemon(pokemon)}
-            >
-              <span className="p-1">{pokemon.name}</span>
-            </Badge>
-          ))}
-        </div>
+        {loading ? (
+          <div className="flex justify-center items-center h-full w-full">
+            <LoadingSpinner size="lg" />
+          </div>
+        ) : (
+          <div className="flex flex-wrap gap-2">
+            {pokemonList.map((pokemon) => (
+              <Badge
+                key={pokemon.name}
+                variant="outline"
+                className={`m-1 cursor-pointer hover:bg-gray-100 ${
+                  selectedPokemon === pokemon.name ? "border-black" : ""
+                } `}
+                onClick={() => togglePokemon(pokemon)}
+              >
+                <span className="p-1">{pokemon.name}</span>
+              </Badge>
+            ))}
+          </div>
+        )}
       </Card>
-      <Card className="p-5 w-1/4 items-center">
+      <Card className="p-5 w-80 items-center">
         <h2 className="text-2xl font-bold">포켓몬 상세정보</h2>
         {selectedPokemon === null ? (
           <>
